88a575c79d651d7dea5ce25c0b07ed42931f6439
Jack Firth <jackhfirth@gmail.com>
2017-07-14 21:52:46 -0700

    Reorganize main module

--- main.rkt
+++ main.rkt
@@ -27,18 +27,40 @@
          syntax/parse/define)
 
 
+;; Kernel API
+
 (struct disposable (proc))
+
+(define (disposable-alloc! disp) ((disposable-proc disp)))
+
+;; Contracts
 
 (define (disposable/c value/c)
   (struct/c disposable (-> (values value/c any/c))))
+
+;; Safe caller interface
+
+(define (call/disposable disp f)
+  (define-values (v dispose!) (disposable-alloc! disp))
+  (begin0 (f v) (dispose!)))
+
+(define-simple-macro (with-disposable ([id:id disp:expr] ...) body:expr ...)
+  (call/disposable (disposable-apply list disp ...)
+                   (λ (vs) (apply (λ (id ...) body ...) vs))))
+
+;; Safe monadic compositional interface
+
+(define (disposable-alloc-all! disps)
+  (define (disposable-alloc/list! disp)
+    (call-with-values (disposable-proc disp) list))
+  (map disposable-alloc/list! disps))
 
 (define (disposable-pure v) (disposable (thunk (values v void))))
 
 (define (disposable-apply f . disps)
   (disposable
    (thunk
-    (define v+dispose!-pairs
-      (map (λ (disp) (call-with-values (disposable-proc disp) list)) disps))
+    (define v+dispose!-pairs (disposable-alloc-all! disps))
     (values (apply f (map first v+dispose!-pairs))
             (thunk
              (for ([dispose! (in-list (map second v+dispose!-pairs))])
@@ -48,18 +70,12 @@
   (define list-disp (apply disposable-apply list disps))
   (disposable
    (thunk
-    (define-values (vs vs-dispose!) ((disposable-proc list-disp)))
-    (define-values (f-v f-dispose!) ((disposable-proc (apply f vs))))
+    (define-values (vs vs-dispose!) (disposable-alloc! list-disp))
+    (define-values (f-v f-dispose!) (disposable-alloc! (apply f vs)))
     (define (dispose-all!) (f-dispose!) (vs-dispose!))
     (values f-v dispose-all!))))
 
+;; Construction sugar
+
 (define (disposable/create+delete create delete)
   (disposable (thunk (define v (create)) (values v (thunk (delete v))))))
-
-(define (call/disposable disp f)
-  (define-values (v dispose!) ((disposable-proc disp)))
-  (begin0 (f v) (dispose!)))
-
-(define-simple-macro (with-disposable ([id:id disp:expr] ...) body:expr ...)
-  (call/disposable (disposable-apply list disp ...)
-                   (λ (vs) (apply (λ (id ...) body ...) vs))))
