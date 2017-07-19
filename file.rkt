5b4f9d6c1123db97bff13afc3a4022a1d236fe54
Jack Firth <jackhfirth@gmail.com>
2017-07-18 22:17:02 -0700

    Rename #:in-dir keyword to #:parent-dir

--- file.rkt
+++ file.rkt
@@ -4,9 +4,10 @@
 
 (provide
  (contract-out
-  [disposable-file (->* () (#:contents string? #:in-dir (or/c #f path-string?))
+  [disposable-file (->* ()
+                        (#:contents string? #:parent-dir (or/c #f path-string?))
                         (disposable/c path?))]
-  [disposable-directory (->* () (#:in-dir (or/c #f path-string?))
+  [disposable-directory (->* () (#:parent-dir (or/c #f path-string?))
                              (disposable/c path?))]))
 
 (require disposable
@@ -14,13 +15,13 @@
          racket/function)
 
 
-(define (disposable-file #:contents [contents ""] #:in-dir [dir #f])
+(define (disposable-file #:contents [contents ""] #:parent-dir [parent-dir #f])
   (define (create-file)
-    (define path (make-temporary-file "rkttmp~a" #f dir))
+    (define path (make-temporary-file "rkttmp~a" #f parent-dir))
     (display-to-file contents path #:mode 'text #:exists 'truncate)
     path)
   (disposable/create+delete create-file delete-file))
 
-(define (disposable-directory #:in-dir [parent-dir #f])
+(define (disposable-directory #:parent-dir [parent-dir #f])
   (define (create-dir) (make-temporary-file "rkttmp~a" 'directory parent-dir))
   (disposable/create+delete create-dir delete-directory))
