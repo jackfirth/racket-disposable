53d09b74282c8f109666b3a76a939a0a263ee8c2
Jack Firth <jackhfirth@gmail.com>
2017-07-19 19:59:00 -0700

    Add tests for `acquire!`

--- private/test.rkt
+++ private/test.rkt
@@ -6,9 +6,21 @@
            disposable/file
            disposable/example
            disposable/testing
-           disposable/unsafe)
-  (check-all-documented 'disposable)
-  (check-all-documented 'disposable/file)
-  (check-all-documented 'disposable/example)
-  (check-all-documented 'disposable/testing)
-  (check-all-documented 'disposable/unsafe))
+           disposable/unsafe
+           rackunit)
+
+  (test-case "acquire!"
+    (define-values (disp get-log)
+      (disposable/event-log (disposable-pure 'foo)))
+    (define-values (v dispose!) (acquire! disp))
+    (check-equal? v 'foo)
+    (check-equal? (get-log) '((alloc foo)))
+    (dispose!)
+    (check-equal? (get-log) '((alloc foo) (dealloc foo))))
+
+  (test-case "documentation coverage of public modules"
+    (check-all-documented 'disposable)
+    (check-all-documented 'disposable/file)
+    (check-all-documented 'disposable/example)
+    (check-all-documented 'disposable/testing)
+    (check-all-documented 'disposable/unsafe)))
