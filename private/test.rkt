3bceb7984f913340d1c325805c70dc61479574a2
Jack Firth <jackhfirth@gmail.com>
2017-07-19 17:54:18 -0700

    Add documentation test

--- private/test.rkt
+++ private/test.rkt
@@ -5,8 +5,10 @@
            disposable
            disposable/file
            disposable/example
+           disposable/testing
            disposable/unsafe)
   (check-all-documented 'disposable)
   (check-all-documented 'disposable/file)
   (check-all-documented 'disposable/example)
+  (check-all-documented 'disposable/testing)
   (check-all-documented 'disposable/unsafe))
