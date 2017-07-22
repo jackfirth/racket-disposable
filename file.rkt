b5d9abc168a8c5ee910089bdbdaa7352b5ae5ec6
Jack Firth <jackhfirth@gmail.com>
2017-07-21 19:23:28 -0700

    Make filesystem disposables idempotent

--- file.rkt
+++ file.rkt
@@ -11,6 +11,7 @@
                              (disposable/c path?))]))
 
 (require disposable
+         disposable/unsafe
          racket/file
          racket/function)
 
@@ -18,12 +19,25 @@
   (require rackunit))
 
 
+(define-logger disposable-file)
+(define-logger disposable-directory)
+
 (define (disposable-file #:contents [contents ""] #:parent-dir [parent-dir #f])
-  (define (create-file)
+  (define (create)
     (define path (make-temporary-file "rkttmp~a" #f parent-dir))
     (display-to-file contents path #:mode 'text #:exists 'truncate)
     path)
-  (disposable create-file delete-file))
+  (define (delete file)
+    (define (exn:no-such-file? exn)
+      (and (exn:fail:filesystem:errno? exn)
+           (member (exn:fail:filesystem:errno-errno exn)
+                   '((2 . posix) (2 . windows)))
+           #t))
+    (define (log-no-such-file _)
+      (define msg-format "attempted to delete nonexistent file: ~a")
+      (log-disposable-file-info msg-format file))
+    (with-handlers ([exn:no-such-file? log-no-such-file]) (delete-file file)))
+  (disposable create delete))
 
 (module+ test
   (test-case "disposable-file"
@@ -34,11 +48,25 @@
     (check-exn exn:fail:filesystem? (thunk (file->string file*))))
   (test-case "disposable-file #:contents"
     (with-disposable ([file (disposable-file #:contents "stuff")])
-      (check-equal? (file->string file) "stuff"))))
+      (check-equal? (file->string file) "stuff")))
+  (test-case "disposable-file idempotent"
+    (define-values (v dispose!) (acquire! (disposable-file)))
+    (dispose!)
+    (check-not-exn dispose!)))
     
 (define (disposable-directory #:parent-dir [parent-dir #f])
   (define (create-dir) (make-temporary-file "rkttmp~a" 'directory parent-dir))
-  (disposable create-dir delete-directory))
+  (define (delete dir)
+    (define (exn:no-such-dir? exn)
+      (and (exn:fail:filesystem:errno? exn)
+           (member (exn:fail:filesystem:errno-errno exn)
+                   '((2 . posix) (3 . windows)))
+           #t))
+    (define (log-no-such-dir _)
+      (define msg-format "attempted to delete nonexistent directory: ~a")
+      (log-disposable-directory-info msg-format dir))
+    (with-handlers ([exn:no-such-dir? log-no-such-dir]) (delete-directory dir)))
+  (disposable create-dir delete))
 
 (module+ test
   (test-case "disposable-directory"
@@ -56,4 +84,8 @@
     (with-disposable ([dir (disposable-directory)])
       (with-disposable ([file (disposable-file #:parent-dir dir)])
         (check-equal? (directory-list dir #:build? #t) (list file)))
-      (check-equal? (directory-list dir) '()))))
+      (check-equal? (directory-list dir) '())))
+  (test-case "disposable-directory idempotent"
+    (define-values (v dispose!) (acquire! (disposable-directory)))
+    (dispose!)
+    (check-not-exn dispose!)))
